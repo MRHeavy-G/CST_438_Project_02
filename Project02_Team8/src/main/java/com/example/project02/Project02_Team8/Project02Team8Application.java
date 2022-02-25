d7b2e8140936f400e68692b20d222990f19da228
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2022-02-24 19:29:37 -0800

    This push will have the Spring boot initilizr and assisten on Intellj
    The community version of intellj didnt work for me to I had to download the the ultimate version. If you have any questions let me know! As well as this Spring boot file is the basics and doestn have anything added for function.

---
+++ Project02_Team8/src/main/java/com/example/project02/Project02_Team8/Project02Team8Application.java
@@ -0,0 +1,13 @@
+package com.example.project02.Project02_Team8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02Team8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project02Team8Application.class, args);
+	}
+
+}
---
+++ Project02_Team8/src/test/java/com/example/project02/Project02_Team8/Project02Team8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project02.Project02_Team8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project02Team8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
